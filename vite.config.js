69ff4cfbcb907443d5ac11c1279a8641077ebb3f
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 12:43:44 +0100

    integrated react appi url

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const REACT_APP_API_URL = import.meta.env.REACT_APP_API_URL
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
     port: 4500,
     proxy: {
       '/api': {
-        target: 'http://localhost:7000', // Replace with your backend server URL
+        target: REACT_APP_API_URL || 'http://localhost:7000', // Replace with your backend server URL
         changeOrigin: true,
         secure: false,
       },  // Proxy API requests to Node.js backend
